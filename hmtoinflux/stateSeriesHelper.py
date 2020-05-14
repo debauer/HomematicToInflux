3b6517f3ee62616b25b6b37fd09426ae77e17733
debauer <debauer@users.noreply.github.com>
2020-05-14 15:55:38 +0200

    added state series helper for HM-ES-TX-WM

--- hmtoinflux/stateSeriesHelper.py
+++ hmtoinflux/stateSeriesHelper.py
@@ -353,3 +353,34 @@ class FALMOTC12StateSeriesHelper(SeriesHelper):
 
         bulk_size = config.influx.bulk_size
         autocommit = True
+
+
+class ESTXWMStateSeriesHelper(SeriesHelper):
+    class Meta:
+        client = InfluxClient
+
+        # The series name must be a string. Add dependent fields/tags
+        # in curly brackets.
+        series_name = 'ESTXWM'
+
+        device_type = DeviceType.ESTXWM.value
+
+        fields = [
+            'UNREACH_0',
+            'STICKY_UNREACH_0',
+            'CONFIG_PENDING_0',
+            'LOWBAT_0',
+            'RSSI_DEVICE_0',
+            'RSSI_PEER_0',
+            'DEVICE_IN_BOOTLOADER_0',
+            'UPDATE_PENDING_0',
+            'GAS_ENERGY_COUNTER_1',
+            'GAS_POWER_1',
+            'ENERGY_COUNTER_1',
+            'POWER_1',
+        ]
+
+        tags = ['DEVICE_NAME']
+
+        bulk_size = config.influx.bulk_size
+        autocommit = True
